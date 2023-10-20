ce2303d75d111b2fdbacf32732fd0a1a5135cb89
Eduardo Miguel Hernández <32711490+edwardc91@users.noreply.github.com>
2023-10-20 09:53:05 -0400

    Added module initial menu and action for form, tree and kanban view for buyers using res.partner views

--- addons/salescave/__manifest__.py
+++ addons/salescave/__manifest__.py
@@ -24,7 +24,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/res_partner_views.xml',
+        'views/salescave_menu.xml',
     ],
 }
