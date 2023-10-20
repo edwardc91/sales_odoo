a62aa42d7bd1a0172731014eca38bcba5bc355f0
Eduardo Miguel Hernández <32711490+edwardc91@users.noreply.github.com>
2023-10-20 02:23:31 -0400

    Added field for image on product model

--- addons/salescave/models/salescave_product.py
+++ addons/salescave/models/salescave_product.py
@@ -8,5 +8,6 @@ class Product(models.Model):
     _description = 'Product purchased to set on sale'
 
     name = fields.Char(string="Nombre del product", required=True)
+    image = fields.Binary(string='Imagen')
     description = fields.Text(string="Descripción")
 
