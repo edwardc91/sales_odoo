d4ec344fbefa53331ac24616c5ed5603a5a5be0d
Eduardo Miguel Hernández <32711490+edwardc91@users.noreply.github.com>
2023-10-24 05:14:54 -0400

    Added compute fields to sale models

--- addons/salescave/models/salescave_sale.py
+++ addons/salescave/models/salescave_sale.py
@@ -14,6 +14,56 @@ class Sale(models.Model):
     
     sales_products_ids = fields.One2many('salescave.sale.product', 'sale_id',
                                    string='Ventas/Productos')
+    
+    currency_id = fields.Many2one('res.currency', string='Moneda de venta')
+    # compute fields
+    total_sale = fields.Monetary(
+        string='Valor total de la venta', compute='_compute_total_sale')
+    
+    total_gain = fields.Monetary(
+        string='Inversión total', compute='_compute_total_gain')
+    
+    total_paid = fields.Monetary(
+        string='Total pagado (en caja)', compute='_compute_total_paid')
+    
+    total_debt = fields.Monetary(
+        string='Total de deuda (en la calle)', compute='_compute_total_debt')
+    
+    @api.depends('sales_products_ids')
+    def _compute_total_sale(self):
+        for record in self:
+            total_sale = 0
+            for sale in record.sales_products_ids:
+                total_sale += sale.total_sale
+                
+            record.total_sale = total_sale
+            
+    @api.depends('sales_products_ids')
+    def _compute_total_gain(self):
+        for record in self:
+            total_gain = 0
+            for sale in record.sales_products_ids:
+                total_gain += sale.total_gain
+                
+            record.total_gain = total_gain
+            
+    @api.depends('sales_products_ids')
+    def _compute_total_paid(self):
+        for record in self:
+            total_paid = 0
+            for sale in record.sales_products_ids:
+                total_gain += sale.total_paid
+                
+            record.total_paid = total_paid
+            
+    @api.depends('sales_products_ids')
+    def _compute_total_debt(self):
+        for record in self:
+            total_debt = 0
+            for sale in record.sales_products_ids:
+                total_debt += sale.debt
+                
+            record.total_debt = total_debt
 
 
 class SaleProduct(models.Model):
@@ -34,12 +84,51 @@ class SaleProduct(models.Model):
     quantity = fields.Integer(
         string='Cantidad vendida', default=1, required=True)
 
-    currency_id = fields.Many2one('res.currency', string='Moneda de compra')
+    currency_id = fields.Many2one('res.currency', string='Moneda de venta')
     sale_price_x_product = fields.Monetary(
         string='Precio venta por producto', required=True)
 
     payments_ids = fields.One2many('salescave.sale.payment', 'sale_product_id',
                                    string='Pagos')
+    
+    # compute fields
+    total_sale = fields.Monetary(
+        string='Valor total de la venta', compute='_compute_total_sale')
+    
+    total_gain = fields.Monetary(
+        string='Ganancia total', compute='_compute_total_gain')
+    
+    total_paid = fields.Monetary(
+        string='Total pagado', compute='_compute_total_paid')
+    
+    debt = fields.Monetary(
+        string='Deuda', compute='_compute_debt')
+    
+    
+    @api.depends('sale_price_x_product', 'quantity')
+    def _compute_total_sale(self):
+        for record in self:
+            record.total_sale = record.sale_price_x_product * record.quantity
+            
+    @api.depends('total_sale', 'product_purchase_id')
+    def _compute_total_gain(self):
+        for record in self:
+            if record.product_purchase_id:
+                record.total_gain = record.total_sale - (record.product_purchase_id.cost_x_product * record.quantity)
+                
+    @api.depends('payments_ids')
+    def _compute_total_paid(self):
+        for record in self:
+            total_paid = 0
+            for payment in record.payments_ids:
+                total_paid += payment.payment
+                
+            record.total_paid = total_paid
+            
+    @api.depends('total_paid', 'total_sale')
+    def _compute_debt(self):
+        for record in self:     
+            record.total_debt = record.total_sale - record.total_paid
 
 
 class SalePayment(models.Model):
@@ -56,6 +145,6 @@ class SalePayment(models.Model):
         'salescave.sale.product', string='Venta/Producto')
     payment_date = fields.Date(string='Fecha de venta', required=True)
 
-    currency_id = fields.Many2one('res.currency', string='Moneda de compra')
+    currency_id = fields.Many2one('res.currency', string='Moneda de venta')
     payment = fields.Monetary(
         string='Precio venta por producto', required=True)
